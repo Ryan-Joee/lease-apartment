b21338f11b58b379edd4e7bcddbbb1ad8cb2f5e2
Ryan-Joee <3497882088@qq.com>
2025-05-11 16:09:25 +0800

    refactor(web-admin): 完成标签信息保存、修改和删除的逻辑
    - 在 saveOrUpdateLabel 方法中添加了对保存或修改结果的判断，确保操作成功后返回 OK，失败则返回 FAIL
    - 在 deleteLabelById 方法中添加了删除结果的判断，确保删除成功后返回 OK，失败则返回 FAIL

--- web/web-admin/src/main/java/com/ryan/lease/web/admin/controller/apartment/LabelController.java
+++ web/web-admin/src/main/java/com/ryan/lease/web/admin/controller/apartment/LabelController.java
@@ -46,13 +46,20 @@ public class LabelController {
     @Operation(summary = "新增或修改标签信息")
     @PostMapping("saveOrUpdate")
     public Result saveOrUpdateLabel(@RequestBody LabelInfo labelInfo) {
-
+        boolean result = service.saveOrUpdate(labelInfo);
+        if (!result) {
+            return Result.fail();
+        }
         return Result.ok();
     }
 
     @Operation(summary = "根据id删除标签信息")
     @DeleteMapping("deleteById")
     public Result deleteLabelById(@RequestParam Long id) {
+        boolean result = service.removeById(id);
+        if (!result) {
+            return Result.fail();
+        }
         return Result.ok();
     }
 }
