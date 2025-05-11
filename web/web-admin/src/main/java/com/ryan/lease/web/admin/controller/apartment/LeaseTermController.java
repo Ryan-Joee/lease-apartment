e759ab25e24efd3430155258f2339036bb4560f1
Ryan-Joee <3497882088@qq.com>
2025-05-11 09:19:27 +0800

    完成租期管理lease_term的三个业务API

--- web/web-admin/src/main/java/com/ryan/lease/web/admin/controller/apartment/LeaseTermController.java
+++ web/web-admin/src/main/java/com/ryan/lease/web/admin/controller/apartment/LeaseTermController.java
@@ -2,9 +2,12 @@ package com.ryan.lease.web.admin.controller.apartment;
 
 
 import com.ryan.lease.common.result.Result;
+import com.ryan.lease.common.result.ResultCodeEnum;
 import com.ryan.lease.model.entity.LeaseTerm;
+import com.ryan.lease.web.admin.service.LeaseTermService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,21 +17,36 @@ import java.util.List;
 @RestController
 public class LeaseTermController {
 
+    @Autowired
+    private LeaseTermService leaseTermService;
+
     @GetMapping("list")
     @Operation(summary = "查询全部租期列表")
     public Result<List<LeaseTerm>> listLeaseTerm() {
-        return Result.ok();
+        List<LeaseTerm> list = leaseTermService.list();
+        if (list.isEmpty()) {
+            return Result.fail();
+        }
+        return Result.ok(list);
     }
 
     @PostMapping("saveOrUpdate")
     @Operation(summary = "保存或更新租期信息")
     public Result saveOrUpdate(@RequestBody LeaseTerm leaseTerm) {
+        boolean result = leaseTermService.saveOrUpdate(leaseTerm);
+        if (!result) {
+            return Result.fail();
+        }
         return Result.ok();
     }
 
     @DeleteMapping("deleteById")
     @Operation(summary = "根据ID删除租期")
     public Result deleteLeaseTermById(@RequestParam Long id) {
+        boolean result = leaseTermService.removeById(id);
+        if (!result) {
+            return Result.fail();
+        }
         return Result.ok();
     }
 }
