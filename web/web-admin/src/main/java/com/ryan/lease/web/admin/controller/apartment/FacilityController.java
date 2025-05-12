d1af8bdab1a695e610de96176997a4aebc3e0434
Ryan-Joee <3497882088@qq.com>
2025-05-12 09:02:58 +0800

    feat(web-admin): 实现配套信息基本操作
    - 新增配套信息列表查询功能，支持按类型筛选
    - 实现配套信息的新增和修改功能
    - 添加配套信息删除功能
    - 优化项目依赖配置，移除不必要的插件

--- web/web-admin/src/main/java/com/ryan/lease/web/admin/controller/apartment/FacilityController.java
+++ web/web-admin/src/main/java/com/ryan/lease/web/admin/controller/apartment/FacilityController.java
@@ -1,7 +1,9 @@
 package com.ryan.lease.web.admin.controller.apartment;
 
 
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.ryan.lease.common.result.Result;
 import com.ryan.lease.model.entity.FacilityInfo;
 import com.ryan.lease.model.enums.ItemType;
@@ -19,21 +21,37 @@ import java.util.List;
 @RequestMapping("/admin/facility")
 public class FacilityController {
 
+    @Autowired
+    private FacilityInfoService service;
+
     @Operation(summary = "[根据类型]查询配套信息列表")
     @GetMapping("list")
     public Result<List<FacilityInfo>> listFacility(@RequestParam(required = false) ItemType type) {
-        return Result.ok();
+        LambdaQueryWrapper<FacilityInfo> list = Wrappers.lambdaQuery(FacilityInfo.class).eq(type != null, FacilityInfo::getType, type);
+        List<FacilityInfo> facilityInfoList = service.list(list);
+        if (facilityInfoList.isEmpty()) {
+            return Result.fail();
+        }
+        return Result.ok(facilityInfoList);
     }
 
     @Operation(summary = "新增或修改配套信息")
     @PostMapping("saveOrUpdate")
     public Result saveOrUpdate(@RequestBody FacilityInfo facilityInfo) {
+        boolean result = service.saveOrUpdate(facilityInfo);
+        if (!result) {
+            return Result.fail();
+        }
         return Result.ok();
     }
 
     @Operation(summary = "根据id删除配套信息")
     @DeleteMapping("deleteById")
     public Result removeFacilityById(@RequestParam Long id) {
+        boolean result = service.removeById(id);
+        if (!result) {
+            return Result.fail();
+        }
         return Result.ok();
     }
 
