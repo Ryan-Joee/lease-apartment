f25eb9728ac0300f977786394da579fe6ca41643
Ryan-Joee <3497882088@qq.com>
2025-05-14 15:07:57 +0800

    feat(web-admin): 完善公寓信息管理功能
    - 添加了根据 ID 修改公寓发布状态的功能
    - 实现了根据区县 ID 查询公寓信息列表的功能

--- web/web-admin/src/main/java/com/ryan/lease/web/admin/controller/apartment/ApartmentController.java
+++ web/web-admin/src/main/java/com/ryan/lease/web/admin/controller/apartment/ApartmentController.java
@@ -1,6 +1,9 @@
 package com.ryan.lease.web.admin.controller.apartment;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.ryan.lease.common.result.Result;
 import com.ryan.lease.model.entity.ApartmentInfo;
@@ -59,13 +62,20 @@ public class ApartmentController {
     @Operation(summary = "根据id修改公寓发布状态")
     @PostMapping("updateReleaseStatusById")
     public Result updateReleaseStatusById(@RequestParam Long id, @RequestParam ReleaseStatus status) {
+        LambdaUpdateWrapper<ApartmentInfo> updateWrapper = Wrappers.lambdaUpdate(ApartmentInfo.class)
+                .eq(ApartmentInfo::getId, id)
+                .set(ApartmentInfo::getIsRelease, status);
+        service.update(updateWrapper);
         return Result.ok();
     }
 
     @Operation(summary = "根据区县id查询公寓信息列表")
     @GetMapping("listInfoByDistrictId")
     public Result<List<ApartmentInfo>> listInfoByDistrictId(@RequestParam Long id) {
-        return Result.ok();
+        LambdaQueryWrapper<ApartmentInfo> queryWrapper = Wrappers.lambdaQuery(ApartmentInfo.class)
+                .eq(ApartmentInfo::getDistrictId, id);
+        List<ApartmentInfo> list = service.list(queryWrapper);
+        return Result.ok(list);
     }
 }
 
