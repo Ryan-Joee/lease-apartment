61393254e021e0e8b77393f489e51717586f526d
Ryan-Joee <3497882088@qq.com>
2025-05-18 09:01:30 +0800

    feat(web-app): 搭建移动端基础代码结构
    - 新增公寓信息、预约看房、浏览历史等功能的控制器、服务层和数据访问层
    - 实现公寓信息详情、预约看房详情等数据结构- 添加属性键值对相关功能，用于描述房间基本属性
    - 集成MinIO对象存储配置

---
+++ web/web-app/src/main/java/com/ryan/lease/web/app/service/LoginService.java
@@ -0,0 +1,4 @@
+package com.ryan.lease.web.app.service;
+
+public interface LoginService {
+}
---
+++ web/web-app/src/main/java/com/ryan/lease/web/app/service/impl/SmsServiceImpl.java
@@ -0,0 +1,9 @@
+package com.ryan.lease.web.app.service.impl;
+
+import com.ryan.lease.web.app.service.SmsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SmsServiceImpl implements SmsService {
+
+}
